6b287b583625e8e1086e7e8954a6f11b77024575
muhammad rio <muhammad.rioperm@gmail.com>
2023-11-15 01:24:02 +0700

    add AddUserUseCase

---
+++ src/Applications/use_cases/AddUserUseCase.ts
@@ -0,0 +1,30 @@
+import UserRepository from "../../Domains/users/UserRepository";
+import PasswordHash from "../security/PasswordHash";
+import { UserPayload } from "../security/lib/definitions";
+import Validator from "../security/validator/Validator";
+import { User } from "../../Domains/entities/User";
+import ValidationError from "../../Common/errors/ValidationError";
+import { randomUUID } from "crypto";
+
+export default class AddUserUseCase {
+  private readonly userRepository: UserRepository
+  private readonly validator: Validator
+  private readonly passwordHash: PasswordHash
+
+  constructor(userRepository: UserRepository, validator: Validator, passwordHash: PasswordHash) {
+    this.userRepository = userRepository
+    this.validator = validator
+    this.passwordHash = passwordHash
+  }
+
+  async execute(payload: UserPayload) {
+    const result = this.validator.validatePayload(payload)
+    const id = randomUUID()
+    const hashedPassword = await this.passwordHash.hash(result.password)
+    return await this.userRepository.addUser({
+      id,
+      ...result,
+      password: hashedPassword,
+    })
+  }
+}
---
+++ src/Applications/use_cases/_test/AddUserUseCase.test.ts
@@ -0,0 +1,60 @@
+import AddUserUseCase from "../AddUserUseCase";
+import { UserPayload } from "../../security/lib/definitions";
+import UserValidator from "../../security/validator/UserValidator";
+import ValidationError from "../../../Common/errors/ValidationError";
+import UserRepository from "../../../Domains/users/UserRepository";
+import PasswordHash from "../../security/PasswordHash";
+
+describe('AddUserUseCase', () => {
+  let userRepositoryMock: jest.Mocked<UserRepository>
+  let validatorMock: jest.Mocked<UserValidator>
+  let passwordHashMock: jest.Mocked<PasswordHash>
+  let addUserUseCase: AddUserUseCase
+
+  beforeEach(() => {
+    userRepositoryMock = {
+      addUser: jest.fn(),
+      getIdByUsername: jest.fn(),
+      getUserByUsername: jest.fn()
+    };
+
+    validatorMock = {
+      validatePayload: jest.fn().mockImplementation((arg: string) => {
+        return new UserValidator().validatePayload(arg)
+      }),
+    };
+  
+    passwordHashMock = {
+      hash: jest.fn().mockImplementation((arg: string) => {
+        return 'hashedpassword'
+      }),
+      comparePassword: jest.fn(),
+    };
+
+    addUserUseCase = new AddUserUseCase(userRepositoryMock, validatorMock, passwordHashMock)
+  })
+
+  it('should validate the payload and return a registered user when the payload is valid', async () => {
+    // Arrange
+    const payload: UserPayload = { fullname: 'John Doe', username: 'johndoe', password: 'password' }
+
+    // Act
+    await addUserUseCase.execute(payload);
+
+    // Assert
+    expect(validatorMock.validatePayload)
+      .toHaveBeenCalledWith({ fullname: 'John Doe', username: 'johndoe', password: 'password' });
+    expect(passwordHashMock.hash).toHaveBeenCalledWith('password');
+    expect(userRepositoryMock.addUser).toHaveBeenCalledTimes(1)
+    expect(userRepositoryMock.addUser)
+    .toHaveBeenCalledWith({ id: expect.any(String), fullname: 'John Doe', username: 'johndoe', password: 'hashedpassword' })
+  });
+
+  it('should throw a ValidationError when the payload is not valid', async () => {
+    // Arrange
+    const payload: UserPayload = { fullname: 'John Doe', username: '', password: '' }
+
+    //Act & Assert
+    await expect(addUserUseCase.execute(payload)).rejects.toThrow(ValidationError)
+  });
+});
