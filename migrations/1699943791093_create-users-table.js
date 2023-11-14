3412197c43fa4bb9e7dc2d9dc571a67938a4893e
muhammad rio <muhammad.rioperm@gmail.com>
2023-11-14 23:14:34 +0700

    add pg migration

---
+++ migrations/1699943791093_create-users-table.js
@@ -0,0 +1,23 @@
+/* eslint-disable camelcase */
+/* istanbul ignore file */
+
+exports.up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'varchar(16)', notNull: true, unique: true, primaryKey: true
+    },
+    fullname: {
+      type: 'text', notNull: true
+    },
+    username: {
+      type: 'varchar(50)', notNull: true, unique: true
+    },
+    password: {
+      type: 'VARCHAR(24)', notNull: true
+    }
+  })
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('users')
+};
