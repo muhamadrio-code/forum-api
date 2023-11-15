c5c9f3995bcec76ea693992e95d06cfb81da2ca7
muhammad rio <muhammad.rioperm@gmail.com>
2023-11-15 19:50:03 +0700

    create table authentications

---
+++ migrations/1700040238429_create-authentication-table.js
@@ -0,0 +1,17 @@
+/* eslint-disable camelcase */
+/* istanbul ignore file */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('authentications', {
+    token: {
+      type: 'TEXT',
+      notNull: true
+    }
+  })
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('authentications')
+};
