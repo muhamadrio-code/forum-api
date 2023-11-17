8a23e07952ba8de5846d23ab260c3869cb11ed09
muhammad rio <muhammad.rioperm@gmail.com>
2023-11-17 17:57:10 +0700

    fix test

--- src/Infrastructures/repository/_test/PostgresAuthenticationRepository.test.ts
+++ src/Infrastructures/repository/_test/PostgresAuthenticationRepository.test.ts
@@ -10,7 +10,7 @@ describe('PostgresAuthenticationRepository', () => {
   beforeAll(async () => {
     await PostgresTestHelper.truncate({
       pool,
-      tables: ['authentications']
+      tableName: 'authentications'
     })
   })
 
@@ -21,7 +21,7 @@ describe('PostgresAuthenticationRepository', () => {
   afterEach(async () => {
     await PostgresTestHelper.truncate({
       pool,
-      tables: ['authentications']
+      tableName: 'authentications'
     })
   })
 
@@ -29,23 +29,19 @@ describe('PostgresAuthenticationRepository', () => {
     await pool.end()
   })
 
-  it('should be instance of AuthenticationRepository', () => {
-    expect(authRepository).toBeInstanceOf(AuthenticationRepository);
-  });
-
   it('should call addToken method without errors', async () => {
     await expect(authRepository.addToken('testToken')).resolves.toBeUndefined();
     await expect(authRepository.verifyToken('testToken')).resolves.not.toThrow(InvariantError)
   });
 
-  it('should call deleteToken with valid token method without errors', async () => {
-    await authRepository.addToken('test_token')
-    await expect(authRepository.deleteToken('test_token')).resolves.toBeUndefined();
-    await expect(authRepository.verifyToken('testToken')).rejects.toThrow(InvariantError)
-  });
-
-  it('should call deleteToken with invalid token method without errors', async () => {
+  it('should run without errors when calling deleteToken with invalid token', async () => {
     await authRepository.addToken('test_token')
     await expect(authRepository.deleteToken('invalid_token')).resolves.toBeUndefined();
+  });
+
+  it('should successfully delete token when calling deleteToken with valid token', async () => {
+    await authRepository.addToken('test_token')
+    await expect(authRepository.deleteToken('test_token')).resolves.toBeUndefined();
+    await expect(authRepository.verifyToken('test_token')).rejects.toThrow(InvariantError)
   });
 });
