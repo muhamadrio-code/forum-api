dd34ecda970bd53c4ee66cb2857dcd192e617fa0
muhammad rio <muhammad.rioperm@gmail.com>
2023-11-17 17:52:59 +0700

    refactor

--- src/Infrastructures/repository/_test/helper/PostgresTestHelper.ts
+++ src/Infrastructures/repository/_test/helper/PostgresTestHelper.ts
@@ -1,18 +1,15 @@
 /* istanbul ignore file */
 import { Pool } from "pg";
 
-type Table = 'users' | 'authentications'
+type TableName = 'users' | 'authentications'
 
 type Config = {
   pool: Pool, 
-  tables: Table[]
+  tableName: TableName
 }
 export const PostgresTestHelper = {
   async truncate(config: Config) {
-    const { pool, tables } = config
-
-    tables.forEach(async (table) => {
-      await pool.query(`TRUNCATE TABLE ${table}`);
-    })
+    const { pool, tableName } = config
+    await pool.query(`TRUNCATE ${tableName}`);
   },
 };
