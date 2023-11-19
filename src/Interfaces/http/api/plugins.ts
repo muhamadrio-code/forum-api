bc83e83c81e4f14faa98f5b3f2cc8526c4df9595
muhammad rio <muhammad.rioperm@gmail.com>
2023-11-19 21:42:20 +0700

    add authentications plugin

--- src/Interfaces/http/api/plugins.ts
+++ src/Interfaces/http/api/plugins.ts
@@ -1,7 +1,9 @@
 /* istanbul ignore file */
 import Hapi from "@hapi/hapi"
 import { usersPlugin } from "./users"
+import { authenticationsPlugin } from "./authentications"
 
 export const plugins: Hapi.Plugin<any>[] = [
-  // usersPlugin
+  usersPlugin,
+  authenticationsPlugin
 ]
