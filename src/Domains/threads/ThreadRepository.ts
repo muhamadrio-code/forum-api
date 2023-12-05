32d7b3d1d8df8a17278aa033f203889895a2a331
muhammad rio <muhammad.rioperm@gmail.com>
2023-12-05 16:20:41 +0700

    mark method as deprecated

--- src/Domains/threads/ThreadRepository.ts
+++ src/Domains/threads/ThreadRepository.ts
@@ -3,5 +3,9 @@ import { AddedThread, Thread, ThreadDetailsEntity, ThreadEntity } from "./entiti
 export default abstract class ThreadRepository {
   abstract addThread(thread: Thread): Promise<AddedThread>
   abstract getThreadById(id: string): Promise<ThreadEntity>
+
+  /**
+   * @deprecated should not use this function
+   */
   abstract getThreadDetails(id: string): Promise<ThreadDetailsEntity>
 }
