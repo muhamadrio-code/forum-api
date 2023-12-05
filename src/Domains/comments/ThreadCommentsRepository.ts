f32c9b18e5cfec712b67345b82a7ef9538fa7a8a
muhammad rio <muhammad.rioperm@gmail.com>
2023-12-05 14:49:27 +0700

    mark methods as deprecated, will remove in next patch

--- src/Domains/comments/ThreadCommentsRepository.ts
+++ src/Domains/comments/ThreadCommentsRepository.ts
@@ -2,8 +2,15 @@ import { Comment, AddedComment, DeletedComment, CommentEntity } from "./entities
 
 export default abstract class ThreadCommentRepository {
   abstract getCommentById(id:string): Promise<CommentEntity>
+  /**
+   * @deprecated The method should not be used
+   */
   abstract getCommentReplyById(id:string): Promise<CommentEntity>
   abstract addComment(comment: Comment): Promise<AddedComment>
   abstract deleteComment(commentId: string): Promise<DeletedComment>
+
+  /**
+   * @deprecated The method should not be used
+   */
   abstract addCommentReply(comment: Comment): Promise<AddedComment>
 }
